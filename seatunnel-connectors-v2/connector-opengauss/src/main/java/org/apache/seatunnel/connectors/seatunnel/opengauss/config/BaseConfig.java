2d2ca42d2fd8d0f50c5b40acd375798bd793cc91
吖彪快跑 <lcbiao34@gmail.com>
2022-10-26 07:25:15 +0800

    connector opengauss init

---
+++ seatunnel-connectors-v2/connector-opengauss/src/main/java/org/apache/seatunnel/connectors/seatunnel/opengauss/config/BaseConfig.java
@@ -0,0 +1,17 @@
+package org.apache.seatunnel.connectors.seatunnel.opengauss.config;
+
+import java.io.Serializable;
+
+/**
+ * BaseConfig
+ *
+ * @author <a href="mailto:lcbiao34@gmail.com">Builder34</a>
+ * @since 2022-10-25 23:36:42
+ */
+public class BaseConfig implements Serializable {
+
+    public static final String URL = "url";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+
+}
---
+++ seatunnel-connectors-v2/connector-opengauss/src/main/java/org/apache/seatunnel/connectors/seatunnel/opengauss/config/SourceConfig.java
@@ -0,0 +1,10 @@
+package org.apache.seatunnel.connectors.seatunnel.opengauss.config;
+
+/**
+ * SourceConfig
+ *
+ * @author <a href="mailto:lcbiao34@gmail.com">Builder34</a>
+ * @since 2022-10-25 23:36:51
+ */
+public class SourceConfig {
+}
